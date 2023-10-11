9e3890bd222d9e5d58b4d816eb9507a19704d22b
JuliaF23 <126780475+JuliaF23@users.noreply.github.com>
2023-10-11 18:10:38 -0300

    Create MassagemBanner.js

---
+++ src/components/MassagemBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Banner.css';
+
+function MassagemBanner() {
+  return (
+    <div className='hero-container'>
+      <img src='/images/BannerMassagem.jpeg' alt="Logo" className="Banner" />
+    </div>
+  );
+}
+
+export default MassagemBanner;
