3b8c7970641e2553003cca0da76fd599190e89bd
JuliaF23 <126780475+JuliaF23@users.noreply.github.com>
2023-10-11 20:29:52 -0300

    Update Banner2.js

--- src/components/Banner2.js
+++ src/components/Banner2.js
@@ -5,7 +5,7 @@ import './Banner.css';
 function Banner2() {
   return (
     <div className='hero-container'>
-      <img src='/images/Banner2.jpeg' alt="Logo" className="Banner" />
+      <img src='/images/BannerLogin.jpeg' alt="Logo" className="Banner" />
     </div>
   );
 }
