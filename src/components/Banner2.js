2088f7373cf19482e2b14056345e0a1f0ffdee94
JuliaF23 <126780475+JuliaF23@users.noreply.github.com>
2023-10-11 18:11:25 -0300

    Create Banner2.js

---
+++ src/components/Banner2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Banner.css';
+
+function Banner2() {
+  return (
+    <div className='hero-container'>
+      <img src='/images/Banner2.jpeg' alt="Logo" className="Banner" />
+    </div>
+  );
+}
+
+export default Banner2;
